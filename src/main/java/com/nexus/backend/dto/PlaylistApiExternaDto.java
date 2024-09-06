c3b900fc408d0ef6c482e3bf2a9e62b5ff911f69
gabrielhgbarreto <gabriel.barreto@sptech.schoool>
2024-09-06 02:38:21 -0300

    Add: DTO's para refenciar responses

--- src/main/java/com/nexus/backend/dto/PlaylistApiExternaDto.java
+++ src/main/java/com/nexus/backend/dto/PlaylistApiExternaDto.java
@@ -1,4 +1,89 @@
 package com.nexus.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PlaylistApiExternaDto {
+
+    @JsonProperty("items")
+    private List<PlaylistItem> items;
+
+    public List<PlaylistItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<PlaylistItem> items) {
+        this.items = items;
+    }
+
+    public List<String> getVideoIds() {
+        return items.stream()
+                .map(item -> item.getSnippet().getResourceId().getVideoId())
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getTitle() {
+        return items.stream()
+                .map(item -> item.getSnippet().getTitle())
+                .collect(Collectors.toList());
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class PlaylistItem {
+
+        @JsonProperty("snippet")
+        private Snippet snippet;
+
+        public Snippet getSnippet() {
+            return snippet;
+        }
+
+        public void setSnippet(Snippet snippet) {
+            this.snippet = snippet;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Snippet {
+
+        @JsonProperty("resourceId")
+        private ResourceId resourceId;
+
+        @JsonProperty("title")
+        private String title;
+
+        public ResourceId getResourceId() {
+            return resourceId;
+        }
+
+        public void setResourceId(ResourceId resourceId) {
+            this.resourceId = resourceId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class ResourceId {
+
+        @JsonProperty("videoId")
+        private String videoId;
+
+        public String getVideoId() {
+            return videoId;
+        }
+
+        public void setVideoId(String videoId) {
+            this.videoId = videoId;
+        }
+    }
 }
--- src/main/java/com/nexus/backend/dto/VideoApiExternaDto.java
+++ src/main/java/com/nexus/backend/dto/VideoApiExternaDto.java
@@ -1,4 +1,73 @@
 package com.nexus.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class VideoApiExternaDto {
+
+    @JsonProperty("items")
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Integer getViews(){
+        return Integer.parseInt(items.get(0).getStatistics().getViewCount());
+    }
+
+    public static class Item {
+
+        @JsonProperty("statistics")
+        private Statistics statistics;
+
+        public Statistics getStatistics() {
+            return statistics;
+        }
+
+        public void setStatistics(Statistics statistics) {
+            this.statistics = statistics;
+        }
+    }
+
+    public static class Statistics {
+
+        @JsonProperty("viewCount")
+        private String viewCount;
+
+        @JsonProperty("likeCount")
+        private String likeCount;
+
+        @JsonProperty("commentCount")
+        private String commentCount;
+
+        public String getViewCount() {
+            return viewCount;
+        }
+
+        public void setViewCount(String viewCount) {
+            this.viewCount = viewCount;
+        }
+
+        public String getLikeCount() {
+            return likeCount;
+        }
+
+        public void setLikeCount(String likeCount) {
+            this.likeCount = likeCount;
+        }
+
+        public String getCommentCount() {
+            return commentCount;
+        }
+
+        public void setCommentCount(String commentCount) {
+            this.commentCount = commentCount;
+        }
+    }
 }
